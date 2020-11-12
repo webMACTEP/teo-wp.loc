bb76b8283d2fd0f1e00f5e94caaff56090aad36a
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2020-11-12 21:54:47 +0300

    субменю

--- wp-content/themes/sp-theme-master/mainPage.php
+++ wp-content/themes/sp-theme-master/mainPage.php
@@ -14,7 +14,7 @@ get_header(); ?>
 </section>
 <section class="about">
 	<div class="container">
-		<div class="about__title"><?php the_field("title01", 8); ?></div>
+		<div class="about__title"><a href="<?php the_field("abautEstetik", 8); ?>"><?php the_field("title01", 8); ?></a></div>
 		<div class="about__item">
 			<a href="<?php the_field("abautEstetik", 8); ?>">
 				<p><?php the_field("text01", 8); ?></p>
@@ -26,7 +26,7 @@ get_header(); ?>
 </section>
 <section class="proects">
 	<div class="container">
-		<div class="proects__title"><?php the_field("proects-title", 8); ?></div>
+		<div class="proects__title"><a href="../proekty/"><?php the_field("proects-title", 8); ?></a></div>
 		<div class="proects__items">
 			<div class="proects__item">
 				<div class="proects__item-title"><?php the_field("ptitle01", 8); ?></div>
@@ -51,7 +51,7 @@ get_header(); ?>
 <section class="about-fond">
 	<div class="container">
 		<div class="about-fond__row">
-			<div class="about-fond__left"><?php the_field("aboutTitle", 8); ?></div>
+			<div class="about-fond__left"><a href="../about/"><?php the_field("aboutTitle", 8); ?></a></div>
 			<div class="about-fond__right"><?php the_field("aboutText", 8); ?></div>
 		</div>
 	</div>
@@ -59,25 +59,43 @@ get_header(); ?>
 </section>
 <section class="news">
 	<div class="container">
-		<div class="news__title"><?php the_field("newsTitle", 8); ?></div>
+		<div class="news__title"><a href="../news/"><?php the_field("newsTitle", 8); ?></a></div>
 		<div class="news__blok">
+			<?php
+			$sp_obj = new SpClass();
+
+			while (have_posts()) : the_post(); ?>
+
+				<div class="page">
+
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+
+				</div>
+
+			<?php endwhile; ?>
+
 			<div class="news__item">
-				<a href="<?php the_field("nLink01", 8); ?>">
+				<!-- <a href="<?php the_field("nLink01", 8); ?>">
 					<div class="news__item-title"><?php the_field("nTitle01", 8); ?></div>
 					<div class="news__item-text"><?php the_field("nText01", 8); ?></div>
-				</a>
+				</a> -->
 				<div class="news__item-img"><img src="<?php the_field("nImg01", 8); ?>" alt=""></div>
 			</div>
-			<div class="news__item"><a href="<?php the_field("nLink02", 8); ?>">
+			<div class="news__item">
+				<!-- <a href="<?php the_field("nLink02", 8); ?>">
 					<div class="news__item-title"><?php the_field("nTitle02", 8); ?></div>
 					<div class="news__item-text"><?php the_field("nText02", 8); ?></div>
-				</a>
+				</a> -->
 				<div class="news__item-img"><img src="<?php the_field("nImg02", 8); ?>" alt=""></div>
 			</div>
-			<div class="news__item"><a href="<?php the_field("nLink03", 8); ?>">
+			<div class="news__item">
+				<!-- <a href="<?php the_field("nLink03", 8); ?>">
 					<div class="news__item-title"><?php the_field("nTitle03", 8); ?></div>
 					<div class="news__item-text"><?php the_field("nText03", 8); ?></div>
-				</a>
+				</a> -->
 				<div class="news__item-img"><img src="<?php the_field("nImg03", 8); ?>" alt=""></div>
 			</div>
 		</div>
@@ -87,7 +105,7 @@ get_header(); ?>
 <section class="media">
 	<div class="container">
 		<div class="media__row">
-			<div class="media__title"><?php the_field("mediaTitle", 8); ?></div>
+			<div class="media__title"><a href="../media/"><?php the_field("mediaTitle", 8); ?></a></div>
 			<div class="media__text-blok">
 				<div class="media__text-title"><?php the_field("mediaSubtitle", 8); ?></div>
 				<div class="media__item"><?php the_field("mediaText", 8); ?></div>
